5140cfaa8ea0f9002aed700a5000d91d3376662d
Lucas DE ANGELIS <deangelis.lucas@outlook.com>
2024-12-04 15:13:26 +0100

    use secure websockets or regular websockets, depending on the current protocol

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
           pkg-config
         ] ++ devTools;
 
-        version = "0.1.0";
+        version = "0.1.1";
         pname = "simple-nats-ui";
 
         # Function to create a docker image with a specific tag
